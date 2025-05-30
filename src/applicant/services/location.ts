383ca5871cd99940687b9023cea048b614ba8fb7
Jose David Garcia <JOSE.GARCIADOWNING@ucr.ac.cr>
2025-05-30 01:03:01 -0600

    se creo la clase ApplicantService que maneja los servicios relacionados al applicante

--- src/applicant/services/location.ts
+++ src/applicant/services/location.ts
@@ -1,15 +1,50 @@
+import { http } from "../../plugins/http-client.plugin";
 
+interface Ubication {
+  countryId: string;
+  countryName: string;
+  regionName: string;
+  regionId: string;
+}
 
+interface BaseApplicantProfile {
+  firstName: string;
+  lastName: string;
+  ubication: Ubication;
+}
 
-export const searchLocation = async ({ url, value }: { url: string; value: string }) => {
-    try {
-        const response = await fetch(`${url}/${value}`);
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
+export const searchLocation = async ({
+  url,
+  value,
+}: {
+  url: string;
+  value: string;
+}) => {
+  try {
+    const response = await fetch(`${url}/${value}`);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
+
+export class ApplicantService {
+  static async createBaseApplicationProfile(data: BaseApplicantProfile) {
+    console.log({ data });
+    try {
+      return await http.postAuth(
+        `https://qp5wg7p4-3000.use2.devtunnels.ms/api/applicant/`,
+        {
+          ...data,
+        }
+      );
+    } catch (error) {
+      console.error("Error al crear el perfil base del solicitante:", error);
+      throw error;
+    }
+  }
+}
