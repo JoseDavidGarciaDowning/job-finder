1360a11e328b0fba68c0f39a67fdd4c530a5fff4
Jose David Garcia <JOSE.GARCIADOWNING@ucr.ac.cr>
2025-05-30 00:59:36 -0600

    se modifico la pagina principal, para que ahora muestre las cards de los trabajos y lea el nombre del usuario del store de zustand y lo muestre

--- src/applicant/pages/Home.tsx
+++ src/applicant/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { IonAvatar, IonContent, IonHeader, IonPage } from "@ionic/react";
 import React from "react";
+import JobCard from "../../components/ui/JobCard";
+import { useApplicantStore } from "../../stores/applicant/applicant.store";
 
 const Home: React.FC = () => {
+  const firstName = useApplicantStore((state) => state.firstName);
   return (
     <IonPage>
       <IonHeader>
@@ -10,7 +13,7 @@ const Home: React.FC = () => {
             <div className="py-2.5">
               <h1 className="text-2xl font-bold text-indigo-900">Hola</h1>
               <h2 className="text-2xl font-bold text-indigo-900">
-                Orlando Diggs.
+                {firstName}
               </h2>
             </div>
             <IonAvatar className="w-9">
@@ -23,7 +26,16 @@ const Home: React.FC = () => {
         </div>
       </IonHeader>
       <IonContent fullscreen>
-        <div className="flex flex-col safe-area-top safe-area-bottom  h-screen bg-[#F9F9F9] min-h-screen overflow-auto "></div>
+        <div className="flex flex-col safe-area-top safe-area-bottom  h-screen bg-[#F9F9F9] min-h-screen overflow-auto ">
+          <JobCard
+            titleJob="Senior Product Designer"
+            companyName="Google Inc"
+          />
+          <JobCard
+            titleJob="Senior Software Developer"
+            companyName="Microsoft"
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
